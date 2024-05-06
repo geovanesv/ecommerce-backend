54f8a56d1924a7709b58d5bd7db0feaef1c61356
geovanesv <geovanesv89@gmail.com>
2024-05-06 10:49:06 -0300

    Add Model Product

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-// import { ExceptionInterceptor } from './common/interceptors/exception.interceptor';
+import { ExceptionInterceptor } from './common/interceptors/exception.interceptor';
 
 /** Starts the application */
 async function bootstrap(): Promise<void> {
